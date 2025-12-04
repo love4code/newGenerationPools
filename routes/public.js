7a2d2c4b7589f2cbc185cb2938c7f3f3caa137d3
Mark Grover <markgrover@Marks-MacBook-Pro.local>
2025-12-03 20:06:06 -0500

    Fix formatting in routes/public.js

--- routes/public.js
+++ routes/public.js
@@ -1,31 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const publicController = require('../controllers/publicController');
-const mediaController = require('../controllers/mediaController');
+const express = require('express')
+const router = express.Router()
+const publicController = require('../controllers/publicController')
+const mediaController = require('../controllers/mediaController')
 
 // Image serving (must be before other routes to avoid conflicts)
-router.get('/api/images/:id/:size', mediaController.serve);
+router.get('/api/images/:id/:size', mediaController.serve)
 
 // Home
-router.get('/', publicController.home);
-router.post('/home-contact', publicController.homeContactSubmit);
+router.get('/', publicController.home)
+router.post('/home-contact', publicController.homeContactSubmit)
 
 // Services
-router.get('/services', publicController.services);
-router.get('/services/:slug', publicController.serviceDetail);
+router.get('/services', publicController.services)
+router.get('/services/:slug', publicController.serviceDetail)
 
 // Portfolio
-router.get('/portfolio', publicController.portfolio);
-router.get('/projects/:slug', publicController.projectDetail);
+router.get('/portfolio', publicController.portfolio)
+router.get('/projects/:slug', publicController.projectDetail)
 
 // Products
-router.get('/products', publicController.products);
-router.get('/products/:slug', publicController.productDetail);
-router.post('/products/:slug/order', publicController.productOrderSubmit);
+router.get('/products', publicController.products)
+router.get('/products/:slug', publicController.productDetail)
+router.post('/products/:slug/order', publicController.productOrderSubmit)
 
 // Contact
-router.get('/contact', publicController.contact);
-router.post('/contact', publicController.contactSubmit);
+router.get('/contact', publicController.contact)
+router.post('/contact', publicController.contactSubmit)
 
-module.exports = router;
-
+module.exports = router
